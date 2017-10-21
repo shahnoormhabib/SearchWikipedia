c552ad0602b8c8124d7884b81c8b244d635659da
shahnoormhabib <shahnoor_h@Yahoo.com>
2017-10-21 15:17:45 -0500

    Add files via upload

--- searchWikipedia.js
+++ searchWikipedia.js
@@ -1,37 +1,34 @@
 'use strict';
-function Search(){
-    var searchFor="";
-    searchFor = document.getElementById('search').value;
-    console.log("Searching for: "+searchFor);
+function getRandomArticle(){
+    $("#results").empty();  
+    $.getJSON('https://en.wikipedia.org/w/api.php?action=query&generator=random&grnnamespace=0&prop=extracts&exchars=500&format=json&callback=?', function(response) {
+    var pages = response.query.pages,
+        id = Object.keys(pages)[0],
+        randomPage = pages[id];
+
+    $('#articleTitle').html(randomPage.title);
+    $('#article').html(randomPage.extract);
+});
 }
-function getArticleList() {
-    var searchFor = "";
-    searchFor = document.getElementById('search').value;
-    console.log("Searching for: "+searchFor);
-    var myHeaders = new Headers();
-        myHeaders= {
-            'Access-Control-Allow-Origin': '*',
-            'Accept': 'application/json', 
-            'Content-Type': 'application/json',
-        }   
-     var myInit = { method: 'GET',
-               headers: myHeaders,
-               mode: 'cors',
-               cache: 'default' };
-    console.log("Searching for: "+searchFor);
-    var finalURL="https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchFor + "&limit=5&format=jsonfm"
-    finalURL="https://en.wikipedia.org/wiki/India";
-    finalURL="https://en.wikipedia.org/w/api.php?action=query&titles="+ searchFor+"&prop=revisions&rvprop=content&format=jsonfm"
-    console.log(finalURL); 
-    fetch(finalURL,
-    myInit).then(function(resp) {
-        console.log("trySearch");
-        return resp.json()
-    }).then(function(data) {
-        console.log(data);
-        document.querySelector.artName.innerText = data.object[1];
-        document.querySelector.textArt.innerText = data.object[0];
-        document.querySelector.href = data.object[2]
-    })
-};
-//https://en.wikipedia.org/w/api.php?action=query&titles=India%20Page&prop=revisions&rvprop=content&format=json
+
+
+   
+function searchdata() {  
+   
+    var q = $("#search").val();  
+    $.getJSON("http://en.wikipedia.org/w/api.php?callback=?",  
+    {  
+        srsearch: q,  
+        action: "query",  
+        list: "search",  
+        format: "json"  
+    },  
+    function(data) {  
+        $("#results").empty();  
+        $("#results").append("Results for <b>" + q + "</b> </br>");  
+        $("#results").append("<div> </div>");  
+        $.each(data.query.search, function(i, item) {  
+            $("#results").append("<div><a href='http://en.wikipedia.org/wiki/" + encodeURIComponent(item.title) + "'>" + item.title + "</a> : " + item.snippet + "</div>");  
+            });  
+       });  
+}    
